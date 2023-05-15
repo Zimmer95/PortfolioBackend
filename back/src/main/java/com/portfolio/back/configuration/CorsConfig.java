9604c0b32e6bda7bddc75761ea42d4c82f353ea3
= <=>
2023-05-14 22:35:25 -0300

    agrego archivo Dockfile 14/05

--- back/src/main/java/com/portfolio/back/configuration/CorsConfig.java
+++ back/src/main/java/com/portfolio/back/configuration/CorsConfig.java
@@ -2,15 +2,16 @@ package com.portfolio.back.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
+@EnableWebMvc
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200/iniciar-sesion")
                 .allowedMethods("*");            
     }
